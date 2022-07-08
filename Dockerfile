a3f98133c4af136fab0e9fd230939366b5134317
Alexandros Stergiakis <alsterg@google.com>
2022-07-08 19:42:54 +0300

    REST api

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.8
 #TODO use pyc
 ADD calculator.py /
 RUN pip install flask
-CMD python /calculator.py
+CMD FLASK_APP=calculator  flask run --port 5001
--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
 class Calculator():
     """Performs the four basic mathematical operations
 
@@ -16,6 +21,7 @@ class Calculator():
           secondOperand - Any number
         """
         return firstOperand + secondOperand
+
     def subtract(self, firstOperand, secondOperand): 
         """Subtracts two numbers together
 
@@ -24,6 +30,7 @@ class Calculator():
           secondOperand - Any number
         """
         return firstOperand - secondOperand
+
     def multiply(self, firstOperand, secondOperand):
         """Multiplies two numbers together
 
@@ -32,6 +39,7 @@ class Calculator():
           secondOperand - Any number
         """
         return firstOperand * secondOperand
+
     def divide(self, firstOperand, secondOperand):   
         """Divides two numbers together
 
@@ -40,3 +48,21 @@ class Calculator():
           secondOperand - Any number
         """
         return firstOperand / secondOperand
+
+c = Calculator()
+
+@app.route('/calculate')
+def calculate():
+    operator = request.args.get('operator')
+    num1 = int(request.args.get('num1'))
+    num2 = int(request.args.get('num2'))
+
+    if operator == 'add':
+        return str(c.add(num1, num2))
+    if operator == 'subtract':
+        return str(c.subtract(num1, num2))
+    if operator == 'multiply':
+        return str(c.multiply(num1, num2))
+    if operator == 'divide':
+        return str(c.divide(num1, num2))
+
