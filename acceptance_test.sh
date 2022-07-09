1266d3eae4a3b583cfbf1f52617b35a027ebea8a
Alexandros Stergiakis <alsterg@google.com>
2022-07-09 23:00:55 +0300

    fix

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # TODO: IP was discovered manually by running: minikube ip
+sleep 3  # wait for pod to start
 test $(curl 192.168.49.2:32301/calculate?operator=add\&num1=2\&num2=3) -eq 5
