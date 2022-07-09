18cbb832e4e6b84b8fde5cdaeacd9b43662887f0
Alexandros Stergiakis <alsterg@google.com>
2022-07-09 22:02:16 +0300

    Kube service

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-test $(curl localhost:5001/calculate?operator=add\&num1=2\&num2=3) -eq 5
+# TODO: IP was discovered manually by running: minikube ip
+test $(curl 192.168.49.2:5001/calculate?operator=add\&num1=2\&num2=3) -eq 5
