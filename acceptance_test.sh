030cf3d15d96ef434fc0d615b38c5a06bc952bae
Alexandros Stergiakis <alsterg@google.com>
2022-07-09 22:44:23 +0300

    fix

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # TODO: IP was discovered manually by running: minikube ip
-test $(curl 192.168.49.2:5001/calculate?operator=add\&num1=2\&num2=3) -eq 5
+test $(curl 192.168.49.2:32301/calculate?operator=add\&num1=2\&num2=3) -eq 5
